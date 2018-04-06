d0d8cfb3334121cc7b0dd6ddfecb92a97a65b6ab
Juan Carlos <jcgamar@unal.edu.co>
2018-04-05 23:18:01 -0500

    historial de prestamos funcionando

--- src/historial/typeDefs.js
+++ src/historial/typeDefs.js
@@ -1,13 +1,13 @@
 export const prestamosTypeDef = `
 type Prestamo {
     id: Int!
-    user_id: Int!
+    student_id: Int!
     bici_id: Int!
     solicitud: String
 }
 
 input PrestamoInput {
-    user_id: Int!
+    student_id: Int!
     bici_id: Int!
     solicitud: String
 }
