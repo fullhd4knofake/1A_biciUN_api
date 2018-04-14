83efcf96f647aa6685e62df7d8b99c3420227b69
Juan Carlos <jcgamar@unal.edu.co>
2018-04-14 10:52:03 -0500

    correccion archivo server profilepicture

--- src/profilepictures/server.js
+++ src/profilepictures/server.js
@@ -1,6 +1,3 @@
-// export const url = process.env.PRESTAMOS_URL || 'localhost';
-// export const port = process.env.PRESTAMOS_PORT || '3002';
-// export const entryPoint = process.env.PRESTAMOS_ENTRY || 'prestamos';
-export const url = '192.168.99.101';
-export const port = '3003';
-export const entryPoint = "profilepictures";
+export const url = process.env.PROFILES_PHOTOS_URL || '192.168.99.101';
+export const port = process.env.PROFILES_PHOTOS_PORT || '3003';
+export const entryPoint = process.env.PROFILES_PHOTOS_ENTRY || 'profilepictures';
