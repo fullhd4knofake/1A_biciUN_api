4d87cbcefcc35db494eb7ec8fc92e2fbc7e40ff8
Juan Carlos <jcgamar@unal.edu.co>
2018-05-01 02:06:33 -0500

    generacion de tokens con auth

--- src/auth/resolvers.js
+++ src/auth/resolvers.js
@@ -1,12 +1,17 @@
-import { generalRequest, getRequest } from '../utilities';
+import { generalRequest, getRequest, generateToken } from '../utilities';
 import { url, port, entryPoint } from './server';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
 
 const resolvers = {
     Mutation: {
-        auth: (_, { auth }) =>
-            generalRequest(`${URL}`, 'POST', auth)
+        auth: async (_, { auth }) => {
+            const resp = await generalRequest(`${URL}`, 'POST', auth)
+            if(resp.answer != "true"){
+                throw "Autenticacion fallo"
+            }
+            return generateToken(resp.id)
+        }
     }
 };
 
