8f062bd6d535b61f8e740dd19e8b678c7177b922
Miguel Tovar <matovaro@unal.edu.co>
2018-05-23 21:27:32 -0500

    server profilepictures

--- src/profilepictures/server.js
+++ src/profilepictures/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.PROFILES_PHOTOS_URL || '192.168.99.101';
+export const url = process.env.PROFILES_PHOTOS_URL || 'profilepictures';
 export const port = process.env.PROFILES_PHOTOS_PORT || '3003';
 export const entryPoint = process.env.PROFILES_PHOTOS_ENTRY || 'profilepictures';
